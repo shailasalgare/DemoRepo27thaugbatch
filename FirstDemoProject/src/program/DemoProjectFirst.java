b4fb50f2e7f20e9cf01c13e975494c31d9c49351
shaila-salgare <118903983+shailasalgare@users.noreply.github.com>
2023-02-02 23:27:50 +0530

    BasicTask

---
+++ FirstDemoProject/src/program/DemoProjectFirst.java
@@ -0,0 +1,16 @@
+package program;
+
+public class DemoProjectFirst {
+
+	public static void main(String[] args) {
+		 System.out.println("Test Base");
+		 System.out.println("Test page layer");
+		 System.out.println("utilities");
+		 System.out.println("extentent report");
+		 System.out.println("Suite");
+		 
+
+
+	}
+
+}
